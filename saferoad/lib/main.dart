2e1e39eeadbe1475f2af94163d683133dcdaabb9
DanlerLo <loaizad65@gmail.com>
2023-02-14 23:38:40 -0500

    Conexiones y port mobil

--- saferoad/lib/main.dart
+++ saferoad/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:saferoad/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.android,
+  );
   runApp(const MyApp());
 }
 
